5d8f38e0af381836b86075ff00a619677686da48
Talyehuda2 <Talyehuda2@gmail.com>
2022-07-31 18:48:49 +0300

    NewsApp Breaking News fragment working

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,7 @@ class Constants {
     companion object {
         const val API_KEY = "d55f7d48e0704fa58dfcad338edbf1d5"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
